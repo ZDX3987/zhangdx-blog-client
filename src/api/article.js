f7b4e61b8c1b5439f690a5ccc26e325e9350c78d
zhangdx <zdx3987@163.com>
2020-11-15 20:31:03 +0800

    修改后端访问url，部署nginx上的bug

--- src/api/article.js
+++ src/api/article.js
@@ -1,10 +1,12 @@
 import http from '../http/index'
 
+const url = '/api/client';
+
 export default {
   getArticleByPage(formData) {
-    return http.get('/api/article/articles', {params: formData});
+    return http.get(url + '/article/articles', {params: formData});
   },
   getArticleById(id) {
-    return http.get('/api/article/article/' + id);
+    return http.get(url + '/article/article/' + id);
   }
 }
