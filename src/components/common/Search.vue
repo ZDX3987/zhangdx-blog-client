70cd0c584654b3c106e62701f1b66faf9ad011b5
zhangdx <zdx3987@163.com>
2021-05-27 20:26:18 +0800

    增加搜索框回车搜索

--- src/components/common/Search.vue
+++ src/components/common/Search.vue
@@ -2,10 +2,8 @@
   <el-row class="search-content" type="flex" justify="center">
     <el-col :xl="8" :lg="10" :md="12" :xs="22" :sm="22">
       <div class="search-form align-middle">
-        <form action="">
-          <input type="text" v-model="searchForm.searchWord" class="search-input" placeholder="请输入关键字"></input>
-          <button type="button" class="search-btn" @click="submitSearch()"><i class="fa fa-search"></i></button>
-        </form>
+        <input type="text" v-model="searchForm.searchWord" class="search-input" placeholder="请输入关键字" @keyup.enter="submitSearch()"></input>
+        <button type="button" class="search-btn" @click="submitSearch()"><i class="fa fa-search"></i></button>
       </div>
     </el-col>
   </el-row>
