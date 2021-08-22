6655f7daa443f4b4f248a3a3a8a81796b5a47905
zhangdx <zdx3987@163.com>
2021-08-22 14:28:16 +0800

    修改axios公共响应的代码

--- src/http/index.js
+++ src/http/index.js
@@ -1,29 +1,50 @@
 import axios from 'axios'
 import Router from '../router/index'
-import {Message} from 'element-ui'
+import {Notification} from 'element-ui'
 import {setStorageItem} from '../util/storage-unit';
+import fa from 'element-ui/src/locale/lang/fa';
 
 const service = axios.create({
     baseURL: '',
     timeout: 15000,
 })
 
-service.interceptors.response.use(response => {
+// 请求计数器
+let requestNum = 0;
+let notification = null;
 
+service.interceptors.request.use(request => {
+
+    requestNum++;
+    return request;
+}, error => {
+    requestNum = 0;
+    Promise.reject(error);
+});
+
+
+service.interceptors.response.use(response => {
+        requestNum--;
         storageToken(response);
         return Promise.resolve(response.data);
     },
     error => {
+        if (notification && requestNum <= 0) {
+            notification.closeAll();
+        }
         let resp = error.response;
         errorHandle(resp);
+        requestNum = 0;
         return Promise.reject(resp);
     });
 
 
 function errorHandle(response) {
-    Message({
-        message: '服务器出现错误，请稍后重试！'
-    });
+    notification = Notification.error({
+        title: '错误（' + response.status + ')',
+        message: '服务器出现错误，请稍后重试！',
+        showClose: false
+    })
     Router.push('/error');
 }
 
